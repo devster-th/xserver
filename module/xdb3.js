f0caa97bf1ba246586a1b91ad9d6d0b234da987e
mutita <mutita.org@gmail.com>
2022-12-08 13:54:27 +0700

    just do it

---
+++ module/xdb3.js
@@ -0,0 +1,19 @@
+// xdb3.js
+/**
+ * make it more secure by each doc saved by each owner, and encrypted. Noone excepts the owner can read it. The admin can see structure of each db but not at the doc level.
+ * 
+ * xdb
+ *  --room (room is db name), usually 1 room for an organization, or each team
+ *    --box (box is collection of docs)
+ *      --doc (each object)
+ *        --data (key:value set of data)
+ */
+
+const fileManager = require('fs')
+
+
+
+function xdb(inpu) {
+
+
+}
